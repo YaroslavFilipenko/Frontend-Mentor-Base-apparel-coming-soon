a1ad38542830f7c86f586a8e16665cf0ef77ebc3
Yaroslav Filipenko <yarikfilipenko@gmail.com>
2023-05-16 18:02:40 +0300

    small changes in error message logic
    Signed-off-by: Yaroslav Filipenko <yarikfilipenko@gmail.com>

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const form = document.getElementById('form');
 const emailInput = document.getElementById('email-input');
 const emailSend = document.getElementById('email-send');
-const errorImage = document.getElementById('error-image');
 const errorLabel = document.getElementById('error-label');
 
 emailSend.addEventListener('click', (e) => {
   const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     
   if (!emailInput.value.match(validRegex)) {
-    errorImage.style.display = "block";
     errorLabel.style.display = "block";
+    emailInput.classList.add("email-input-error");
   } else {
     form.reset(); //clear form after click on the send button
   }
@@ -17,6 +16,6 @@ emailSend.addEventListener('click', (e) => {
 });
 
 emailInput.addEventListener('focus', () => {
-    errorImage.style.display = "none";
-    errorLabel.style.display = "none";
+  emailInput.classList.remove("email-input-error");
+  errorLabel.style.display = "none";
 });  
